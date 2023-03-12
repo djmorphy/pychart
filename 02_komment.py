6052ef081c1b5baae7070eb1a71793d659cebf62
djmorphy <djmorphy@gmail.com>
2023-03-12 13:42:20 +0100

    practice comments and varriables

---
+++ 02_komment.py
@@ -0,0 +1,9 @@
+#egysoros komment
+
+"""
+    A hármas idézőjel a több soros komment
+"""
+
+'''
+    A hármas szimpla idézőjel is több soros komment   
+'''
---
+++ 02_valtozok.py
@@ -0,0 +1,13 @@
+
+szam1 = 10
+szam2 = 20
+szam3 = 3.14
+_szam = 46
+_szam_ = 455
+_szam_54 = 453
+
+print(szam1 + szam2)
+
+szam1 = 87
+
+print(szam1)
