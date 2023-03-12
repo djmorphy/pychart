2f4617972198e2b13802611c07f5b2c4243228a0
djmorphy <djmorphy@gmail.com>
2023-03-12 14:18:52 +0100

    practice comparison or relation operators

---
+++ 07_relacios_op.py
@@ -0,0 +1,18 @@
+#comparison or relation operators - összehasonlító vagy relációs operátorok - boolean operátorok
+
+# < >  <= >= != ==
+
+szam1 = 10
+szam2 = 4
+
+print(szam1 < szam2)
+print(szam1 > szam2)
+
+print( 5 <= 5)
+print( 5 >= 5)
+print(14 != 5)
+
+
+print(87 == 87)
+print(87 == 88)
+print(szam1 == szam2)
