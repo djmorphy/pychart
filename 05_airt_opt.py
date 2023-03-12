51b4b91272d4461d905b92cd832cc6d110338e65
djmorphy <djmorphy@gmail.com>
2023-03-12 14:04:31 +0100

    practice aritmetic operators

---
+++ 05_airt_opt.py
@@ -0,0 +1,13 @@
+# + - * /  //(egesz szamos osztas) **(exponencialis)  %(modulo)
+
+szam1 = 5
+szam2 = 10
+
+print(szam1 + szam2)
+print(szam1 - szam2)
+print(szam1 * szam2)
+print(szam1 / szam2)
+print("")
+print(szam1 // szam2)
+print(szam1 ** szam2)
+print(szam2 % szam1)
