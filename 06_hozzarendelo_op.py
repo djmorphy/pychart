70eab555fba1ec427477cfb3da2485d654b2b24d
djmorphy <djmorphy@gmail.com>
2023-03-12 14:11:29 +0100

    practice assigment operators

---
+++ 06_hozzarendelo_op.py
@@ -0,0 +1,18 @@
+#assignment  operator
+# =  +=  -= *= /=   //=  %=  **=
+
+szam = 10
+print(szam)
+
+
+# szam = szam + 1
+szam += 1
+print(szam)
+
+# szam = szam -1
+szam -= 1
+print(szam)
+
+szam *= 40
+print(szam)
+
