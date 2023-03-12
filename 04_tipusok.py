a5f08c7c6191c0d6c7181dabf8402ce1c1d50207
djmorphy <djmorphy@gmail.com>
2023-03-12 13:58:06 +0100

    practice types

---
+++ 04_tipusok.py
@@ -0,0 +1,31 @@
+#Numbers, String, List, Tuple, Dictionary, boolean, user defined
+
+#number
+szam1 = 10
+szam2 = 20.4
+szam3 = 32.3e18 #Scientific notation
+szam4 = .976j   #complex number
+
+#String
+nev = "djmorphy"
+hajszin = 'fekete'
+mondas = 'ki koran kel aranyat lel és kávé kell'
+
+#List
+
+lista = ["alma", "korte"]
+
+#Tuple
+my_tuple = ()
+
+#Dictionary
+my_dict = {}
+
+#boolean
+igaz = True
+hamis = False
+
+#változó típus lekérdezése
+print(type(szam1))
+print(type(szam3))
+print(type(szam4))
