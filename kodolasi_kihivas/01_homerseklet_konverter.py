85af5dfd3f545907bd20ff42073a331834f5f1d6
djmorphy <djmorphy@gmail.com>
2023-03-14 21:42:44 +0100

    celsius farenheit converter

---
+++ kodolasi_kihivas/01_homerseklet_konverter.py
@@ -0,0 +1,32 @@
+
+"""
+Az első konvertáljon ceslsiusból farenheitbe.
+A második konvertáljon farenheitből ceslsiusba.
+A visszadott értékek egy tizedes számot tartalmazzanak.
+A két formula: 
+f = c × 9/5 + 32
+c = (f - 32) * 5/9
+
+Test értékek
+>>>cel_to_far(30)
+86
+>>>cel_to_far(40)
+104
+>>>far_to_cel(30)
+-1.1
+>>>far_to_cel(40)
+4.4
+"""
+
+def cel_to_far(c):
+    return c * 9/5 + 32
+
+print(cel_to_far(30))
+print(cel_to_far(40))
+
+
+def far_to_cel(f):
+    return round((f - 32) * 5/9, 1)
+
+print(far_to_cel(30))
+print(far_to_cel(40))
